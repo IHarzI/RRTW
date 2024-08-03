65921b002da7fc4a2a38fab923d89c1cb326b8dc
IHarzI <za2ha4r@gmail.com>
2024-08-03 14:05:11 +0300

    Added comment label, updated README

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# RTW_Trilogy-master
+# RRTW
+Realtime ray-tracer, maded as experiment/learning project.
+@2024 (IHarzI) Maslianka Zakhar
+Basic logic is from Ray Tracing in One Weekend.
+For now, ray-tracer is multithreaded, Window native api used as output Window, with possible custom output to PPm image(not multhithreaded).
+WIP. 
--- RTWCore/source/RTW_Types.h
+++ RTWCore/source/RTW_Types.h
@@ -1,3 +1,10 @@
+// RRTW
+//Realtime ray - tracer, maded as experiment / learning project.
+//@2024 (IHarzI)Maslianka Zakhar
+//Basic logic is from Ray Tracing in One Weekend.
+//For now, ray - tracer is multithreaded, Window native api used as output Window, with possible custom output to PPm image(not multhithreaded).
+//WIP.
+
 #pragma once
 
 using uint8 = unsigned char;
